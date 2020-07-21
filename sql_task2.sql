75a813d5235f599e6deb5dd2e4271a0280d2c3a4
salih <sagezgin@gmail.com>
2020-07-21 23:58:10 +0200

    sql_task_2

--- sql_task2.sql
+++ sql_task2.sql
@@ -10,7 +10,7 @@ FROM albums
 JOIN tracks ON albums.AlbumId = tracks.AlbumId;
 
 cevap 3:
-SELECT tracks.Name, tracks.AlbumId, albums.Title, tracks.Milliseconds
+SELECT tracks.Name, tracks.AlbumId, albums.Title, min(tracks.Milliseconds)
 FROM tracks
 JOIN albums ON tracks.AlbumId = albums.AlbumId
 GROUP BY albums.Title
@@ -18,7 +18,15 @@ ORDER BY tracks.Milliseconds DESC;
 
 
 cevap 4:
-SELECT tracks.Milliseconds, albums.Title
+SELECT sum(tracks.Milliseconds), albums.Title, tracks.Name, albums.AlbumId
 FROM tracks
 JOIN albums ON albums.AlbumId = tracks.AlbumId
+GROUP BY albums.Title
 ORDER BY tracks.Milliseconds DESC;
+
+cevap 5:
+SELECT sum(tracks.Milliseconds), albums.Title, tracks.Name, albums.AlbumId
+FROM tracks
+JOIN albums ON albums.AlbumId = tracks.AlbumId
+GROUP BY tracks.Milliseconds > 252000
+ORDER BY tracks.Milliseconds;
